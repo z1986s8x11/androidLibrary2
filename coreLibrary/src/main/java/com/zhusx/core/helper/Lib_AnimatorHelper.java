84710fda37956d1ba95f23ef19bacfb9ffdec016
z1986s8x11 <z1986s8x11@163.com>
2016-12-16 11:14:09 +0800

    init

--- coreLibrary/src/main/java/com/zhusx/core/helper/Lib_AnimatorHelper.java
+++ coreLibrary/src/main/java/com/zhusx/core/helper/Lib_AnimatorHelper.java
@@ -2,10 +2,10 @@ package com.zhusx.core.helper;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.app.Activity;
 import android.content.Context;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.zhusx.core.utils._Views;
@@ -19,99 +19,88 @@ public class Lib_AnimatorHelper {
     protected FrameLayout rootView;
 
     public Lib_AnimatorHelper(Activity activity) {
-        ViewGroup decor = (ViewGroup) activity.getWindow().getDecorView();
-        ViewGroup decorChild = (ViewGroup) decor.getChildAt(0);
-        decor.removeView(decorChild);
-        rootView = new FrameLayout(activity);
-        decor.addView(rootView);
-        rootView.addView(decorChild);
-    }
-
-    public AnimView getAnimView(IMakeAnimView makeAnimView) {
-        return new AnimView(makeAnimView.makeView(rootView.getContext()));
+        rootView = (FrameLayout) activity.findViewById(android.R.id.content);
     }
 
     public interface IMakeAnimView {
         View makeView(Context context);
     }
 
-    public class AnimView {
-        private View animView;
+    private View getAnimView(IMakeAnimView makeAnimView) {
+        View v = makeAnimView.makeView(rootView.getContext());
+        if (v.getParent() != null) {
+            throw new IllegalArgumentException("animView 构造器参数不能有父类  ");
+        }
+        if (v.getMeasuredHeight() == 0 || v.getMeasuredWidth() == 0) {
+            _Views.measureView(v);
+        }
+        rootView.addView(v, v.getMeasuredWidth(), v.getMeasuredHeight());
+        return v;
+    }
 
-        public AnimView(View v) {
-            if (v.getParent() != null) {
-                throw new IllegalArgumentException("animView 构造器参数不能有父类  ");
+    public void startMove(View fromView, View toView, IMakeAnimView makeAnimView) {
+        int[] location = new int[2];
+        fromView.getLocationOnScreen(location);
+        int[] location1 = new int[2];
+        toView.getLocationOnScreen(location1);
+        final View animView = getAnimView(makeAnimView);
+        animView.setX(fromView.getX());
+        animView.setY(fromView.getY());
+        ObjectAnimator.ofFloat(animView, "translationY", location[1], location1[1]).start();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(animView, "translationX", fromView.getLeft(), toView.getLeft());
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                animView.setVisibility(View.VISIBLE);
             }
-            if (v.getMeasuredHeight() == 0 || v.getMeasuredWidth() == 0) {
-                _Views.measureView(v);
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                rootView.removeView(animView);
             }
-            rootView.addView(v, v.getMeasuredWidth(), v.getMeasuredHeight());
-            this.animView = v;
-        }
 
-        public void startMove(View fromView, View toView) {
-            int[] location = new int[2];
-            fromView.getLocationOnScreen(location);
-            int[] location1 = new int[2];
-            toView.getLocationOnScreen(location1);
-            animView.setX(animView.getX());
-            animView.setY(animView.getY());
-            ObjectAnimator.ofFloat(animView, "translationY", location[1], location1[1]).start();
-            ObjectAnimator animator = ObjectAnimator.ofFloat(animView, "translationX", fromView.getLeft(), toView.getLeft());
-            animator.addListener(new Animator.AnimatorListener() {
-                @Override
-                public void onAnimationStart(Animator animation) {
-                    animView.setVisibility(View.VISIBLE);
-                }
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                rootView.removeView(animView);
+            }
 
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    animView.setVisibility(View.GONE);
-                }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+        animator.start();
+    }
 
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                    animView.setVisibility(View.GONE);
-                }
+    public void startTop(View fromView, int top, IMakeAnimView makeAnimView) {
+        int[] location = new int[2];
+        fromView.getLocationOnScreen(location);
+        final View animView = getAnimView(makeAnimView);
+        animView.setX(fromView.getX() + (fromView.getMeasuredWidth() - animView.getMeasuredWidth()) / 2);
+        animView.setY(fromView.getY());
 
-                @Override
-                public void onAnimationRepeat(Animator animation) {
-                }
-            });
-            animator.start();
-        }
+        PropertyValuesHolder translationY = PropertyValuesHolder.ofFloat("translationY", location[1] - fromView.getMeasuredHeight(), location[1] - fromView.getMeasuredHeight() - top);
+        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 1, 0);
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(animView, translationY, alpha);
+        animator.setDuration(1000);
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
 
-        public void startTop(View fromView, int top) {
-            int[] location = new int[2];
-            fromView.getLocationOnScreen(location);
-            animView.setX(fromView.getX() + (fromView.getMeasuredWidth() - animView.getMeasuredWidth()) / 2);
-            animView.setY(fromView.getY());
-            ObjectAnimator.ofFloat(animView, "translationY", location[1], location[1] - top).start();
-            ObjectAnimator animator = ObjectAnimator.ofFloat(animView, "alpha", 1, 0);
-            animator.addListener(new Animator.AnimatorListener() {
-                @Override
-                public void onAnimationStart(Animator animation) {
-                    animView.setAlpha(1.0f);
-                    animView.setVisibility(View.VISIBLE);
-                }
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                rootView.removeView(animView);
+            }
 
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    animView.setVisibility(View.GONE);
-                    animView.setAlpha(1.0f);
-                }
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                rootView.removeView(animView);
+            }
 
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                    animView.setVisibility(View.GONE);
-                    animView.setAlpha(1.0f);
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animation) {
-                }
-            });
-            animator.start();
-        }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+        animator.start();
     }
 }
--- test/src/main/java/com/zhusx/test/Main_Activity.java
+++ test/src/main/java/com/zhusx/test/Main_Activity.java
@@ -1,26 +1,45 @@
 package com.zhusx.test;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
-import com.zhusx.core.app._PublicActivity;
-import com.zhusx.show.ui.P_ProjectFragment;
-import com.zhusx.test.test.A_Activity;
+import com.zhusx.core.helper.Lib_AnimatorHelper;
+import com.zhusx.core.utils._Densitys;
 
-public class Main_Activity extends AppCompatActivity {
+public class Main_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSource(view);
+            }
+        });
     }
 
+    Lib_AnimatorHelper helper;
+
     public void goSource(View v) {
-        Intent intent = new Intent(this, _PublicActivity.class);
-        intent.putExtra(_PublicActivity._EXTRA_FRAGMENT, P_ProjectFragment.class);
-        startActivity(intent);
-        new A_Activity();
+        if (helper == null) {
+            helper = new Lib_AnimatorHelper(this);
+        }
+        helper.startTop(v, _Densitys.dip2px(this, 30), new Lib_AnimatorHelper.IMakeAnimView() {
+            @Override
+            public View makeView(Context context) {
+                TextView t = new TextView(context);
+                t.setText("+1");
+                return t;
+            }
+        });
+//        Intent intent = new Intent(this, _PublicActivity.class);
+//        intent.putExtra(_PublicActivity._EXTRA_FRAGMENT, P_ProjectFragment.class);
+//        startActivity(intent);
+//        new A_Activity();
     }
 }
