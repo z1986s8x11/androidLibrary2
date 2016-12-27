482cac924d142a33d9b05fa5f2896cf1370de0fa
z1986s8x11 <z1986s8x11@163.com>
2016-12-27 12:28:34 +0800

    init

--- show/src/main/java/com/zhusx/show/Lib_SourceCodeManager.java
+++ show/src/main/java/com/zhusx/show/Lib_SourceCodeManager.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +12,9 @@ import android.widget.ListView;
 
 import com.zhusx.core.adapter.Lib_BaseAdapter;
 import com.zhusx.core.utils._Activitys;
-import com.zhusx.core.utils._Lists;
 import com.zhusx.core.widget.slidingmenu.SlidingMenu;
 import com.zhusx.show.html.P_UncaughtException;
+import com.zhusx.show.ui.P_ProjectClassFragment;
 import com.zhusx.show.ui.P_SourceCodeFragment;
 import com.zhusx.show.ui._PublicActivity;
 
@@ -36,55 +34,60 @@ public class Lib_SourceCodeManager {
         final String highlightPackageName = builder.highlightPackageName;
         application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
             @Override
-            public void onActivityCreated(Activity activity, Bundle bundle) {
-
+            public void onActivityCreated(final Activity activity, Bundle bundle) {
+                List<Class> list;
+                if (activity instanceof _PublicActivity) {
+                    if (activity.getIntent() == null) {
+                        return;
+                    }
+                    Class serializable = (Class) activity.getIntent().getSerializableExtra(_PublicActivity._EXTRA_FRAGMENT);
+                    if (serializable == null || serializable == P_ProjectClassFragment.class || serializable == P_SourceCodeFragment.class) {
+                        return;
+                    }
+                    list = new ArrayList<>();
+                    list.add(serializable);
+                } else {
+                    if (!activity.getClass().getName().startsWith(highlightPackageName)) {
+                        return;
+                    }
+                    list = new ArrayList<>();
+                    list.add(activity.getClass());
+                }
+//                if (activity instanceof FragmentActivity) {
+//                    List<Fragment> fragments = ((FragmentActivity) activity).getSupportFragmentManager().getFragments();
+//                    if (!_Lists.isEmpty(fragments)) {
+//                        for (int i = 0; i < fragments.size(); i++) {
+//                            list.add(fragments.get(i).getClass());
+//                        }
+//                    }
+//                }
+                SlidingMenu mSlidingMenu = new SlidingMenu(activity, SlidingMenu.SLIDING_WINDOW);
+                ListView listView = new ListView(activity);
+                listView.setAdapter(new Lib_BaseAdapter<Class>(list) {
+                    @Override
+                    public View getView(LayoutInflater inflater, final Class bean, int position, View convertView, ViewGroup parent) {
+                        View[] vs = _getViewArrays(convertView, parent, R.layout.lib_list_item_1);
+                        _toTextView(vs[0]).setText(bean.getSimpleName());
+                        vs[0].setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent in = new Intent(v.getContext(), _PublicActivity.class);
+                                in.putExtra(_PublicActivity._EXTRA_FRAGMENT, P_SourceCodeFragment.class);
+                                in.putExtra(_Activitys._EXTRA_String_ID, "java/" + bean.getName().replace(".", "/") + ".java");
+                                in.putExtra(_Activitys._EXTRA_String, highlightPackageName);
+                                activity.startActivity(in);
+                            }
+                        });
+                        return vs[0];
+                    }
+                });
+                mSlidingMenu.setMenu(listView);
+                mSlidingMenu.setBehindWidth(600);
+                mSlidingMenu.setMode(SlidingMenu.RIGHT);
             }
 
             @Override
             public void onActivityStarted(final Activity activity) {
-                if (!activity.getClass().getName().startsWith(highlightPackageName)) {
-                    return;
-                }
-                View contentLayout = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
-                if (contentLayout == null) {
-                    return;
-                }
-                if (!SlidingMenu.class.getSimpleName().equals(contentLayout.getClass().getSimpleName())) {
-                    List<Class> list = new ArrayList<>();
-                    list.add(activity.getClass());
-                    if (activity instanceof FragmentActivity) {
-                        List<Fragment> fragments = ((FragmentActivity) activity).getSupportFragmentManager().getFragments();
-                        if (!_Lists.isEmpty(fragments)) {
-                            for (int i = 0; i < fragments.size(); i++) {
-                                list.add(fragments.get(i).getClass());
-                            }
-                        }
-                    }
-                    SlidingMenu mSlidingMenu = new SlidingMenu(activity, SlidingMenu.SLIDING_CONTENT);
-                    ListView listView = new ListView(activity);
-                    listView.setAdapter(new Lib_BaseAdapter<Class>(list) {
-                        @Override
-                        public View getView(LayoutInflater inflater, final Class bean, int position, View convertView, ViewGroup parent) {
-                            View[] vs = _getViewArrays(convertView, parent, R.layout.lib_list_item_1);
-                            _toTextView(vs[0]).setText(bean.getSimpleName());
-                            vs[0].setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Intent in = new Intent(v.getContext(), _PublicActivity.class);
-                                    in.putExtra(_PublicActivity._EXTRA_FRAGMENT, P_SourceCodeFragment.class);
-                                    in.putExtra(_Activitys._EXTRA_String_ID, "java/" + bean.getName().replace(".", "/") + ".java");
-                                    in.putExtra(_Activitys._EXTRA_String, highlightPackageName);
-                                    activity.startActivity(in);
-                                }
-                            });
-                            return vs[0];
-                        }
-                    });
-                    mSlidingMenu.setMenu(listView);
-                    mSlidingMenu.setBehindWidth(600);
-                    mSlidingMenu.setMode(SlidingMenu.RIGHT);
-                }
-
             }
 
             @Override
