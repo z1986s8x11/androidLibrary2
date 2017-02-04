de18554410b3d0a0122bd524e962028c85a1f749
z1986s8x11 <z1986s8x11@163.com>
2017-02-04 11:19:47 +0800

    init

--- coreLibrary/src/main/java/com/zhusx/core/app/Lib_BasePopupWindow.java
+++ coreLibrary/src/main/java/com/zhusx/core/app/Lib_BasePopupWindow.java
@@ -1,5 +1,7 @@
 package com.zhusx.core.app;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.PopupWindow;
 
@@ -9,6 +11,40 @@ import android.widget.PopupWindow;
  * Created      2016/6/29 9:56
  */
 public class Lib_BasePopupWindow extends PopupWindow {
+    public Lib_BasePopupWindow(Context context) {
+        super(context);
+    }
+
+    public Lib_BasePopupWindow(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Lib_BasePopupWindow(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Lib_BasePopupWindow(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public Lib_BasePopupWindow() {
+    }
+
+    public Lib_BasePopupWindow(View contentView) {
+        super(contentView);
+    }
+
+    public Lib_BasePopupWindow(int width, int height) {
+        super(width, height);
+    }
+
+    public Lib_BasePopupWindow(View contentView, int width, int height) {
+        super(contentView, width, height);
+    }
+
+    public Lib_BasePopupWindow(View contentView, int width, int height, boolean focusable) {
+        super(contentView, width, height, focusable);
+    }
 
     @Override
     public void showAtLocation(final View parent, final int gravity, final int x, final int y) {
