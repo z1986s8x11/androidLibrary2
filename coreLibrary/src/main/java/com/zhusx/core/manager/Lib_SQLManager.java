822ced8af17f60300f1215bd1d2d689772605441
z1986s8x11 <z1986s8x11@163.com>
2017-02-22 17:51:48 +0800

    init

--- coreLibrary/src/main/java/com/zhusx/core/manager/Lib_SQLManager.java
+++ coreLibrary/src/main/java/com/zhusx/core/manager/Lib_SQLManager.java
@@ -55,68 +55,6 @@ public class Lib_SQLManager {
      */
     public static final String PrimaryKey = " PRIMARY KEY(?,?) ";
 
-    public enum SQLKeyEnum {
-        /**
-         * 什么都不做
-         */
-        DEFAULT(""),
-        /**
-         * 不能等于Null
-         */
-        NO_NULL("NOT NULL"),
-        /**
-         * 主键
-         */
-        PRIMARY_KEY_AUTOINCREMENT("PRIMARY KEY AUTOINCREMENT"),
-        /**
-         * 不能重复
-         */
-        UNIQUE("UNIQUE");
-        private String value;
-
-        SQLKeyEnum(String value) {
-            this.value = value;
-        }
-
-        public String toString() {
-            return value;
-        }
-    }
-
-    public enum SQLDefaultEnum {
-        /**
-         * 什么都不做
-         */
-        DEFAULT(""),
-        /**
-         * 默认为空
-         */
-        NULL("NULL"),
-        /** 默认当前日期 */
-        // CURRENT_DATE("(datetime(CURRENT_TIMESTAMP,'localtime'))"),
-        /**
-         * 默认为true
-         */
-        TRUE("TRUE"),
-        /**
-         * 默认为false
-         */
-        FALSE("FALSE"),
-        /**
-         * -1
-         */
-        NegativeOne("-1");
-        private String value;
-
-        SQLDefaultEnum(String value) {
-            this.value = value;
-        }
-
-        public String toString() {
-            return value;
-        }
-    }
-
     /**
      * 当前日期的前后多少天日期
      */
