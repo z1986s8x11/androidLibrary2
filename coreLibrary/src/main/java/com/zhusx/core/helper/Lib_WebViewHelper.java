bdb27e297d97981771e378697b1faeba80a755f1
z1986s8x11 <z1986s8x11@163.com>
2017-02-24 15:20:08 +0800

    init

--- coreLibrary/src/main/java/com/zhusx/core/helper/Lib_WebViewHelper.java
+++ coreLibrary/src/main/java/com/zhusx/core/helper/Lib_WebViewHelper.java
@@ -6,6 +6,8 @@ import android.webkit.WebView;
 
 import com.zhusx.core.debug.LogUtil;
 
+import java.util.Locale;
+
 /**
  * Author       zhusx
  * Email        327270607@qq.com
@@ -13,6 +15,8 @@ import com.zhusx.core.debug.LogUtil;
  */
 public class Lib_WebViewHelper {
     private WebView mWebView;
+    public String errorMessage = "网络不稳定或已断开";
+    public String retryButtonText = "您可以点击刷新按钮，再次加载";
 
     @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
     public Lib_WebViewHelper(WebView mWebView) {
@@ -23,7 +27,7 @@ public class Lib_WebViewHelper {
 
     public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
         if (LogUtil.DEBUG) {
-            LogUtil.e(this, String.format("%d:%s:%s", errorCode, description, failingUrl));
+            LogUtil.e(this, String.format(Locale.CHINA, "%d:%s:%s", errorCode, description, failingUrl));
         }
         StringBuffer sb = new StringBuffer();
         sb.append("<html>");
@@ -32,14 +36,14 @@ public class Lib_WebViewHelper {
         sb.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /> ");
         sb.append("<script>");
         sb.append("function reLoad(){");
-        sb.append("window.zhusx.reLoad(\"" + failingUrl + "\");");
+        sb.append("window.zhusx.libReLoad(\"" + failingUrl + "\");");
         sb.append("}");
         sb.append("</script>");
         sb.append("</head>");
         sb.append("<body style=\"position:fixed;top:50%;left:50%;text-align:center;-webkit-transform:translateX(-50%) translateY(-50%);\">");
         //sb.append("<img src=\"file:///android_res/drawable/netbreak.png\" style=\"width:40%\"/>");
-        sb.append("<p style=\"margin-bottom:0px\">网络不稳定或已断开</p>");
-        sb.append("<p style=\"margin-top:10px;color:#a5a5a5;font-size:80%;width:200px;\">您可以点击刷新按钮，再次加载</p>");
+        sb.append("<p style=\"margin-bottom:0px\">" + errorMessage + "</p>");
+        sb.append("<p style=\"margin-top:10px;color:#a5a5a5;font-size:80%;width:200px;\">" + retryButtonText + "</p>");
         sb.append("<p style=\"margin-top:10px;padding:10px;-webkit-border-radius:5px;background-color:#e42a2d;color:#ffffff;\" onclick=\"reLoad()\">刷新页面</p>");
         sb.append("</body>");
         sb.append("</html>");
@@ -49,7 +53,7 @@ public class Lib_WebViewHelper {
     boolean isFinish = true;
 
     @JavascriptInterface
-    public void reLoad(final String url) {
+    public void libReLoad(final String url) {
         if (!isFinish) {
             return;
         }
