7908faba8a37c7e8aee6168b318590c3fae36e5b
z1986s8x11 <z1986s8x11@163.com>
2017-03-10 10:55:12 +0800

    init

--- coreLibrary/src/main/java/com/zhusx/core/utils/_Permissions.java
+++ coreLibrary/src/main/java/com/zhusx/core/utils/_Permissions.java
@@ -1,12 +1,14 @@
 package com.zhusx.core.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationManagerCompat;
 
 /**
  * 用户Android 6.0权限验证
@@ -73,6 +75,36 @@ public class _Permissions {
         Intent intent = new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
         intent.setData(Uri.parse("package:" + activity.getPackageName()));
         activity.startActivity(intent);
+    }
+
+    public static boolean isNotificationEnabled(Context context) {
+        return NotificationManagerCompat.from(context).areNotificationsEnabled();
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+//            final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+//            final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+//            AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Activity.APP_OPS_SERVICE);
+//            ApplicationInfo appInfo = context.getApplicationInfo();
+//            String pkg = context.getPackageName();
+//            int uid = appInfo.uid;
+//            try {
+//                Class<?> appOpsClass = Class.forName(AppOpsManager.class.getName());
+//                Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+//                Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+//                int value = (int) opPostNotificationValue.get(Integer.class);
+//                return ((int) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+//            } catch (ClassNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (IllegalAccessException e) {
+//                e.printStackTrace();
+//            } catch (NoSuchMethodException e) {
+//                e.printStackTrace();
+//            } catch (InvocationTargetException e) {
+//                e.printStackTrace();
+//            } catch (NoSuchFieldException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return false;
     }
 
     public interface PermissionRequest {
