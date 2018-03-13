92ea0387ef560eab61be6125ed418be220f7af06
z1986s8x11 <z1986s8x11@163.com>
2018-03-13 11:40:55 +0800

    init

--- coreLibrary/src/main/java/com/zhusx/core/widget/view/Lib_Widget_TextView.java
+++ coreLibrary/src/main/java/com/zhusx/core/widget/view/Lib_Widget_TextView.java
@@ -2,7 +2,13 @@ package com.zhusx.core.widget.view;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Build;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -14,6 +20,9 @@ import com.zhusx.core.helper.Lib_ShapeHelper;
  * Created       2016/12/30 14:31
  */
 public class Lib_Widget_TextView extends TextView {
+    private int mLineY;
+    private int mViewWidth;
+    private boolean isJustifyText;
 
     public Lib_Widget_TextView(Context context) {
         super(context);
@@ -38,5 +47,90 @@ public class Lib_Widget_TextView extends TextView {
 
     private void init(Context context, AttributeSet attrs) {
         Lib_ShapeHelper.initShapeDrawable(this, context, attrs);
+    }
+
+    public void _setJustifyText(boolean isJustifyText) {
+        this.isJustifyText = isJustifyText;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (!isJustifyText) {
+            super.onDraw(canvas);
+            return;
+        }
+        TextPaint paint = getPaint();
+        paint.setColor(getCurrentTextColor());
+        paint.drawableState = getDrawableState();
+        mViewWidth = getMeasuredWidth();
+        String text = getText().toString();
+        mLineY = 0;
+        mLineY += getTextSize();
+        Layout layout = getLayout();
+        // layout.getLayout()在4.4.3出现NullPointerException
+        if (layout == null) {
+            return;
+        }
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        int textHeight = (int) (Math.ceil(fm.descent - fm.ascent));
+        textHeight = (int) (textHeight * layout.getSpacingMultiplier() + layout.getSpacingAdd());
+        for (int i = 0; i < layout.getLineCount(); i++) {
+            int lineStart = layout.getLineStart(i);
+            int lineEnd = layout.getLineEnd(i);
+            float width = StaticLayout.getDesiredWidth(text, lineStart,
+                    lineEnd, getPaint());
+            String line = text.substring(lineStart, lineEnd);
+            if (needScale(line)) {
+                if (i == layout.getLineCount() - 1) {
+                    canvas.drawText(line, 0, mLineY, paint);
+                } else {
+                    drawScaledText(canvas, lineStart, line, width);
+                }
+            } else {
+                canvas.drawText(line, 0, mLineY, paint);
+            }
+            mLineY += textHeight;
+        }
+    }
+
+    private void drawScaledText(Canvas canvas, int lineStart, String line, float lineWidth) {
+        float x = 0;
+        if (isFirstLineOfParagraph(lineStart, line)) {
+            String blanks = "  ";
+            canvas.drawText(blanks, x, mLineY, getPaint());
+            float bw = StaticLayout.getDesiredWidth(blanks, getPaint());
+            x += bw;
+            line = line.substring(3);
+        }
+
+        int gapCount = line.length() - 1;
+        int i = 0;
+        if (line.length() > 2 && line.charAt(0) == 12288 && line.charAt(1) == 12288) {
+            String substring = line.substring(0, 2);
+            float cw = StaticLayout.getDesiredWidth(substring, getPaint());
+            canvas.drawText(substring, x, mLineY, getPaint());
+            x += cw;
+            i += 2;
+        }
+
+        float d = (mViewWidth - lineWidth) / gapCount;
+        for (; i < line.length(); i++) {
+            String c = String.valueOf(line.charAt(i));
+            float cw = StaticLayout.getDesiredWidth(c, getPaint());
+            canvas.drawText(c, x, mLineY, getPaint());
+            x += cw + d;
+        }
+    }
+
+    private boolean isFirstLineOfParagraph(int lineStart, String line) {
+        return line.length() > 3 && line.charAt(0) == ' ' && line.charAt(1) == ' ';
+    }
+
+    private boolean needScale(String line) {
+        if (TextUtils.isEmpty(line)) {
+            return false;
+        } else {
+            return line.charAt(line.length() - 1) != '\n';
+        }
     }
 }
