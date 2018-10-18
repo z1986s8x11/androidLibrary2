c71716fbadbc4f5fe583b08cd057049f0011dfd2
zhusx <z1986s8x11@163.com>
2018-10-18 15:04:47 +0800

    init

--- test/src/main/java/com/zhusx/test/AppApplication.java
+++ test/src/main/java/com/zhusx/test/AppApplication.java
@@ -1,8 +1,15 @@
 package com.zhusx.test;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.os.Binder;
+import android.preference.PreferenceManager;
 
 import com.zhusx.core.debug.LogUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -12,9 +19,48 @@ import com.zhusx.core.debug.LogUtil;
  */
 
 public class AppApplication extends Application {
+    public static AppApplication application;
+    public static String HOST;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        application = this;
         LogUtil.DEBUG = true;
+        add(new A() {
+            @Override
+            public void bind(Binder binder) {
+                HOST = binder.bind("域名", HOST);
+            }
+        });
+    }
+
+    public void add(A a) {
+        Binder b = new Binder();
+        a.bind(b);
+    }
+
+    public interface A {
+        void bind(Binder binder);
+    }
+
+    static Map<String, String[]> map = new HashMap<>();
+
+    public static class Binder {
+        SharedPreferences sp;
+
+        public Binder() {
+            sp = PreferenceManager.getDefaultSharedPreferences(application);
+        }
+
+        public String bind(String name, String value) {
+            map.put(name, new String[]{value});
+            return sp.getString(name, value);
+        }
+
+        public String bind(String name, String[] value) {
+            map.put(name, value);
+            return sp.getString(name, value[0]);
+        }
     }
 }
